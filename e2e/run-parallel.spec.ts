f50690b159d8f863d8df3ff01f81bda89fd7b8e8
Huyen nguyen <huyennguyen@techsoft.asia>
2026-06-12 20:42:15 +0700

    test: update tc

--- e2e/run-parallel.spec.ts
+++ e2e/run-parallel.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test.describe('SmokeTesting', () => {
 
   // Test 1
-  test('Test 1', async ({ page }) => {
+  test('Test 1', {tag: ['@PlaywrightWithJenkins']} ,async ({ page }) => {
     await page.goto('https://example.com');
     await expect(page).toHaveTitle('Example Domain');
   });
@@ -12,20 +12,20 @@ test.describe('SmokeTesting', () => {
   test.describe('RegressionTesting', () => {
 
     // Test 2
-    test('Test 2', async ({ page }) => {
+    test('Test 2', {tag: ['@PlaywrightWithJenkins']}, async ({ page }) => {
       await page.goto('https://example.com');
       await expect(page).toHaveTitle('Example Domain');
     })
 
     // Test 3
-    test('Test 3', async ({ page }) => {
+    test('Test 3', {tag: ['@PlaywrightWithJenkins']}, async ({ page }) => {
       await page.goto('https://example.com');
       await expect(page).toHaveTitle('Example Domain');
     });
   }); // <-- đóng describe RegressionTesting
 
   // Test 4
-  test('Test 4', async ({ page }) => {
+  test('Test 4', {tag: ['@PlaywrightWithJenkins']}, async ({ page }) => {
     await page.goto('https://example.com');
     await expect(page).toHaveTitle('Example Domains');
   });
